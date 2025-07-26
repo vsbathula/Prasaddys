a151766c516a94ac331ab0f4a9178416b14155e3
Venkata Bathula <ven.sprdy@gmail.com>
2025-07-26 15:33:56 -0500

    adding source path to package.swift

--- Package.swift
+++ Package.swift
@@ -26,10 +26,9 @@ let package = Package(
             // Targets can depend on other targets in this package and products from dependencies.
             .target(
                 name: "prasaddys",
-                dependencies: ["Alamofire"]),
-//                dependencies: [
-//                    .product(name: "Alamofire", package: "Alamofire")
-//                ]),
+                dependencies: ["Alamofire"],
+                path: "Sources/prasaddys",
+                ),
             .testTarget(
                 name: "prasaddysTests",
                 dependencies: ["prasaddys"])
--- Sources/prasaddys/Models/AppConstants.swift
+++ Sources/prasaddys/Models/AppConstants.swift
@@ -2,20 +2,20 @@ import Foundation
 
 public struct AppConstants {
     public struct Keychain {
-        static let userEmailService = "UserEmail"
-        static let userEmailAccount = "AuthenticatedUserEmail"
-        static let userIdService = "UserIdService"
-        static let userIdAccount = "UserId"
-        static let deviceService = "com.ramyam.device"
-        static let deviceAccount = "deviceUUID"
-        static let plexService = "PlexService"
-        static let accessTokenAccount = "accessToken"
-        static let refreshTokenAccount = "refreshToken"
-        static let accessTokenExpiryAccount = "accessTokenExpiry"
-        static let userModelService = "OsModelServicce"
-        static let userModelAccount = "OsModelAccount"
-        static let userSystemVersionService = "OsSystemVersionService"
-        static let userSystemVersionAccount = "OsSystemVersionAccount"
+        public static let userEmailService = "UserEmail"
+        public static let userEmailAccount = "AuthenticatedUserEmail"
+        public static let userIdService = "UserIdService"
+        public static let userIdAccount = "UserId"
+        public static let deviceService = "com.ramyam.device"
+        public static let deviceAccount = "deviceUUID"
+        public static let plexService = "PlexService"
+        public static let accessTokenAccount = "accessToken"
+        public static let refreshTokenAccount = "refreshToken"
+        public static let accessTokenExpiryAccount = "accessTokenExpiry"
+        public static let userModelService = "OsModelServicce"
+        public static let userModelAccount = "OsModelAccount"
+        public static let userSystemVersionService = "OsSystemVersionService"
+        public static let userSystemVersionAccount = "OsSystemVersionAccount"
     }
 }
 
