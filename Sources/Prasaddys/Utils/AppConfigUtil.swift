2582874981f68d769623a1d800be133542f805b2
Venkata Bathula <ven.sprdy@gmail.com>
2025-08-08 14:57:37 -0500

    Update AppConfigUtil.swift

--- Sources/Prasaddys/Utils/AppConfigUtil.swift
+++ Sources/Prasaddys/Utils/AppConfigUtil.swift
@@ -1,36 +1,37 @@
 import Foundation
 
 public class AppConfigUtil {
-
+    
     // MARK: - Critical Configurations (Uses fatalError if missing)
-
+    
     public static func getApiUrl() -> String {
         return getCriticalInfoPlistValue(forKey: "API_URL")
     }
-
+    
     public static func getAuthUrl() -> String {
         return getCriticalInfoPlistValue(forKey: "AUTH_URL")
     }
-
+    
     public static func getClientId() -> String {
         return getCriticalInfoPlistValue(forKey: "CLIENT_ID")
     }
-
+#if os(iOS) || os(macOS)
     public static func getRedirectUri() -> String {
         return getCriticalInfoPlistValue(forKey: "REDIRECT_URI")
     }
-
+#endif
+    
     // MARK: - Non-Critical Configurations (Returns Optional String)
     public static func getYtApiUrl() -> String? {
         return getOptionalInfoPlistValue(forKey: "YT_API_URL")
     }
-
+    
     public static func getYtApiKey() -> String? {
         return getOptionalInfoPlistValue(forKey: "YT_API_KEY")
     }
-
+    
     // MARK: - Private Helper Functions
-
+    
     /// Fetches a critical Info.plist value. Will cause a fatalError if the key is missing or not a String.
     private static func getCriticalInfoPlistValue(forKey key: String) -> String {
         guard let value = Bundle.main.object(forInfoDictionaryKey: key) as? String else {
@@ -38,7 +39,7 @@ public class AppConfigUtil {
         }
         return value
     }
-
+    
     /// Fetches an optional Info.plist value. Returns nil if the key is missing or not a String.
     private static func getOptionalInfoPlistValue(forKey key: String) -> String? {
         guard let value = Bundle.main.object(forInfoDictionaryKey: key) as? String else {
