b0e8bb6d5226edef69c603ecf132e9ef5bd1ab02
Venkata Bathula <ven.sprdy@gmail.com>
2025-08-21 22:21:52 -0500

    Update AuthManager.swift

--- Sources/Prasaddys/Network/AuthManager.swift
+++ Sources/Prasaddys/Network/AuthManager.swift
@@ -336,10 +336,36 @@ public class AuthManager: NSObject {
             throw AuthError.missingTokenData
         }
         
+        guard let email = KeyChainUtil.getUserEmail(), !email.isEmpty else { return }
+        
+        guard let userid = KeyChainUtil.getUserId(), !userid.isEmpty else { return }
+                
+        guard let deviceUuid = KeyChainUtil.getDeviceUuid(), !deviceUuid.isEmpty else { return }
+//        if let email = KeyChainUtil.getUserEmail(), !email.isEmpty {
+//            bodyParams["email"] = email
+//        } else {
+//            print("⚠️ [AuthManager] Warning: User email not found in Keychain for refresh request. Proceeding without it.")
+//        }
+        
+//        if let userid = KeyChainUtil.getUserId(), !userid.isEmpty {
+//            bodyParams["userid"] = userid
+//        } else {
+//            print("⚠️ [AuthManager] Warning: User id not found in Keychain for refresh request. Proceeding without it.")
+//        }
+//        
+//        if let deviceUuid = KeyChainUtil.getDeviceUuid(), !deviceUuid.isEmpty {
+//            bodyParams["deviceid"] = deviceUuid
+//        } else {
+//            print("⚠️ [AuthManager] Warning: User device uuid not found in Keychain for refresh request. Proceeding without it.")
+//        }
+        
         let request = createTokenRequest(with: [
             "grant_type": "refresh_token",
             "refresh_token": refreshToken,
-            "client_id": clientId
+            "client_id": clientId,
+            "email": email,
+            "userid": userid,
+            "deviceid": deviceUuid
         ])
         
         let (data, response) = try await URLSession.shared.data(for: request)
